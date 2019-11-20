5232407217da7f3103dfaf550d2b1bd4aa1faf32
CJH9004 <2015210256@mail.hfut.edu.cn>
2019-11-20 18:10:08 +0800

    add tag

--- parse.go
+++ parse.go
@@ -1,5 +1,26 @@
 package mock
 
-type Tags
+import (
+	"strings"
+	"fmt"
+)
 
-func parseTags(tags string) {}
+// mock:"min(0) max(10) type('string') value(1,3,4) required mock(func) valid(func)" valid:""
+
+var TypeList = []string{"eamil", "date", "phone", "url", "ipv4", "domain"}
+
+type Tags struct {
+	Type      string
+	Values    []interface{}
+	Min       int64 // default 1
+	Max       int64 // default 10
+	MockFunc  MockFunc
+	ValidFunc ValidFunc
+}
+
+func parseTags(tags string) (t Tags) {
+	for _, v := range strings.Fields(tags) {
+		fmt.Println(v)
+	}
+	return
+}
